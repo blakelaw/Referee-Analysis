194c764cce3bbabb42ee8cbe9adde19cd19e9af9
blakelaw <blakelaw@gatech.edu>
2023-09-17 16:24:31 -0400

    initial push

--- NBA_PCA.R
+++ NBA_PCA.R
@@ -1,8 +1,5 @@
-install.packages("knitr")
-library(knitr)
-
 # Load the data
-data_for_pca <- read.csv('/Users/blakelaw/Library/CloudStorage/OneDrive-Personal/PycharmProjects/NBA/PCA.csv', header = TRUE)
+data_for_pca <- read.csv('/Users/blakelaw/PycharmProjects/RefereeAnalysis/Outputs/PCA.csv', header = TRUE)
 
 # Exclude the unnecessary columns for PCA including the "Unnamed: 0" and "x" columns
 data_pca_only <- data_for_pca[, !(names(data_for_pca) %in% c('Unnamed: 0', 'first_name', 'last_name', 'official_id', 'num_games', 'X'))]
@@ -18,13 +15,14 @@ data_with_scores <- cbind(data_for_pca, scores)
 
 
 # If you wish to save the dataframe with normalized scores:
-write.csv(data_with_scores, '/Users/blakelaw/Library/CloudStorage/OneDrive-Personal/PycharmProjects/NBA/PCA_results.csv', row.names = FALSE)
+write.csv(data_with_scores, '/Users/blakelaw/PycharmProjects/RefereeAnalysis/Outputs/PCA_results.csv', row.names = FALSE)
 
 # Print the loadings to understand the weightings for each principal component
 print(pca_result$rotation)
 
 print(data_with_scores)
 
+#Create table for blog post showing loadings
 rounded_rotation <- round(pca_result$rotation, 3)
 table_output <- kable(rounded_rotation, format = "html", caption = "PCA Loadings")
 writeLines(table_output, "/Users/blakelaw/Downloads/my_table.html")
---
+++ convert_notebook.py
@@ -0,0 +1,17 @@
+import nbformat
+
+# Load the notebook
+notebook = nbformat.read('metrics.ipynb', as_version=4)
+
+# Initialize an empty string to store the code
+code_script = ""
+
+# Loop through each cell in the notebook
+for cell in notebook.cells:
+    # If the cell is a code cell, extract the source
+    if cell.cell_type == "code":
+        code_script += cell.source + "\n\n"
+
+# Save the code to a new .py file
+with open('metrics.py', 'w') as f:
+    f.write(code_script)
